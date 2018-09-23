ff742bb0acaf4f3147c48cde58e60fb94998c4e0
felbinger <nico.felbinger2000@gmail.com>
2018-09-23 20:12:46 +0200

    Modified settings.py for docker deployment.

--- GameMicroservice/settings.py
+++ GameMicroservice/settings.py
@@ -1,6 +1,13 @@
-debug = False #TODO set to False PRODUCTION!!!
-database = 'sqlite:///games.sqlite'
-port = 80 #1337
+import os
+
+debug = False
+port = 80
+db_host = os.environ['DB_HOSTNAME']
+db_port = os.environ['DB_PORT']
+db_user = os.environ['DB_USERNAME']
+db_pass = os.environ['DB_PASSWORD']
+db_name = os.environ['DB_DATABASE']
+database = f'mysql+pymysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}?charset=utf8mb4'
+# database = 'sqlite:///users.sqlite'
 
 account_management_url = "https://iam.battleofai.net/api/" #Only local: "http://0.0.0.0:1338/api/"
-
