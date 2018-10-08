6226cdf2be3538e9105f3353d68998f76ba2f2e7
Vincent Schmandt <vincent.schmandt@gmx.de>
2018-10-08 19:37:14 +0200

    Update games.js

--- FrontPage/center/apis/games/games.js
+++ FrontPage/center/apis/games/games.js
@@ -1,23 +1,29 @@
 function display_all_my_games(userid) {
-    loading = "<h1 id='loading_games'>Loading games..</h1>";
-    $("#gamelist").append(loading);
-
-    let my_url = get_games_url() + 'api/games/?user_ids='+userid;
-    $.ajax({url:my_url,
-        type:"get",
-        contentType:"application/json; charset=utf-8",
+    let my_url = get_games_url() + 'api/games/?user_ids=' + userid;
+    $.ajax({
+        url: my_url,
+        type: "get",
+        contentType: "application/json; charset=utf-8",
         cache: false,
         success: function (data, status, xhr) {
-            display_all_games(data);
+            display_game(data.games.map(x => x.id));
         }
     });
 }
-function display_all_games(data) {
-    data["games"] = data["games"].reverse()
-    for (let i = 0; i < data["games"].length; i++){
-        display_game(data["games"][i]["id"]);
-    }
-    $("#loading_games").remove()
+
+function display_game(game_ids) {
+    let current = game_ids.shift(-1);
+    let my_url = get_games_url() + 'api/games/' + current;
+    $.ajax({
+        url: my_url,
+        type: "get",
+        contentType: "application/json; charset=utf-8",
+        cache: false,
+        success: function (data, status, xhr) {
+            console.log(current, data); //TODO do something with the data
+            if (game_ids.length > 0) display_game(game_ids);
+        }
+    });
 }
 
 function display_game(game_id) {
@@ -26,7 +32,6 @@ function display_game(game_id) {
         type:"get",
         contentType:"application/json; charset=utf-8",
         cache: false,
-        async: false,
         success: function (data, status, xhr) {
 
            game_text = "<li class='game'><span class='time'>"+ data['id'] + "</span><span class='title'>"+ data['game_name'] + "</span><span class='icon'> <a href='game.html?id="+ data['id'] + "' class='flag'><i class='fa fa-play-circle'></i></a></span></li>";
